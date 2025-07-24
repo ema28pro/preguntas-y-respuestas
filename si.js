1b3d2a7bb0ebfb474a4729559f4aca807956522c
Emanuel Lopez <ema2805pro@gmail.com>
2025-07-23 23:51:45 -0500

    Agregar archivos iniciales: HTML, CSS, imágenes y un archivo de actividades

---
+++ si.js
@@ -0,0 +1,26 @@
+/*
+let contador = 1;
+
+while (contador <= 10) {
+    console.log(contador);
+    contador ++;
+}
+*/
+
+// let contador = 10;
+
+// while (true) {
+//     if (contador > 0) {
+//         contador --
+//     } else {
+//         break
+//     }
+//     console.log(contador)
+// }
+
+// for (let contador = 10; contador >= 0; contador--){
+//     console.log(contador)
+// }
+
+let numero = opener
+
